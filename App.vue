6345684754e4bd6cad20ed005fa3283e6b2955dc
mrondi <maurorondinelli@hotmail.com>
2018-12-27 04:16:21 -0300

    Initial commit

---
+++ App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <h1>Sens3</h1>
+    <sense></sense>
+  </div>
+</template>
+
+<script>
+import Sense from "./components/Sense.vue";
+
+export default {
+  components: {
+    Sense
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./variables.scss";
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $vue-blue;
+}
+
+h1 {
+  text-align: center;
+}
+
+input#message {
+  padding: 10px;
+  width: 80%;
+  font-size: 15px;
+  border-radius: 2px;
+  border: 1px solid #f2f2f2;
+}
+
+#results {
+  background: #f2f2f2;
+  padding: 35px 20px 30px 20px;
+}
+
+#phrase {
+  font-size: 18px;
+  text-transform: capitalize;
+  font-style: italic;
+  margin-bottom: 30px;
+}
+
+#result-bar span {
+  font-size: 28px;
+  font-weight: bold;
+  display: inline-block;
+  margin: 0px 10px;
+}
+</style>
---
+++ components/Sense.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <center>
+      <input
+        id="message"
+        value=""
+        v-model="message"
+        @keyup.enter="submitMessage();"
+      />
+    </center>
+    <br />
+    <div></div>
+    <div v-if="result" id="results">
+      <center id="phrase">"{{ messageSended }}"</center>
+
+      <div id="result-bar">
+        <center>
+          <span>-</span>
+          <input
+            type="range"
+            min="-3"
+            max="3"
+            v-bind:value="result.score"
+            class="slider"
+            id="myRange"
+          />
+          <span>+</span>
+        </center>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      message: "the life is beautiful and awesome, it's make my happy",
+      messageSended: "",
+      result: ""
+    };
+  },
+  methods: {
+    submitMessage() {
+      this.messageSended = this.message;
+      const { SentimentAnalyzer } = require("node-nlp");
+
+      const sentiment = new SentimentAnalyzer({ language: "en" });
+      this.result = sentiment.getSentiment(this.messageSended);
+      console.log(this.result); // Score: -2, Comparative: -0.666
+    }
+  }
+};
+</script>
